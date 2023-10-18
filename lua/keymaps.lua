25049bd57648515a53d4392bc0339649b3a69ae9
macunan <macunan@gmail.com>
2023-10-18 07:40:42 -0700

    Update keymaps.lua
    Ported all my vimscript to lua to make it more dynamics and reactive

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,8 @@
 -- Personal Vim key settings
 local opts = { noremap = true, silent = true }
-vim.cmd.colorscheme 'peaksea'
-vim.cmd.set('nocompatible')
+-- vim.cmd.colorscheme 'peaksea'
+vim.cmd("colorscheme peaksea")
+vim.cmd('set nocompatible')
 vim.opt.path = vim.opt.path + "**"
 vim.api.nvim_set_keymap("n","<leader>w",":w!<CR>",opts)
 vim.api.nvim_set_keymap("n","<leader>tn",":tabnew<cr>",opts)
