d7068a8f805fe9862a82f3870663fe849bbabae0
macunan <macunan@gmail.com>
2024-11-02 22:48:36 -0300

    Update init.lua
    Added enhanced autocomplete experience for developers

--- init.lua
+++ init.lua
@@ -85,6 +85,7 @@ require('lazy').setup({
   'tpope/vim-dadbod',
 'kristijanhusak/vim-dadbod-ui',
 'kristijanhusak/vim-dadbod-completion',
+'nvim-tree/nvim-web-devicons',
 
 
 
@@ -194,6 +195,28 @@ require('lazy').setup({
       vim.cmd.colorscheme 'peaksea'
     end,
   },
+{
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+        "Myzel394/jsonfly.nvim",
+    },
+    keys = {
+        {
+            "<leader>j",
+            "<cmd>Telescope jsonfly<cr>",
+            desc = "Open json(fly)",
+            ft = { "json", "xml", "yaml" },
+            mode = "n"
+        }
+    }
+},
+
+{
+  -- amongst your other plugins
+  {'akinsho/toggleterm.nvim', version = "*", config = true}
+  -- or
+},
+
 
   {
     -- Set lualine as statusline
@@ -241,6 +264,7 @@ require('lazy').setup({
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
+-- "Myzel394/jsonfly.nvim",
       -- Fuzzy Finder Algorithm which requires local dependencies to be built.
       -- Only load if `make` is available. Make sure you have the system
       -- requirements installed.
@@ -254,6 +278,9 @@ require('lazy').setup({
         end,
       },
     },
+
+
+
   },
 
   {
@@ -609,3 +636,65 @@ cmp.setup {
 -- vim: ts=2 sts=2 sw=2 et
 require('keymaps')
 require("nvim-tree").setup()
+require("telescope").load_extension("jsonfly")
+require'nvim-web-devicons'.setup {
+ -- your personnal icons can go here (to override)
+ -- you can specify color or cterm_color instead of specifying both of them
+ -- DevIcon will be appended to `name`
+ override = {
+  zsh = {
+    icon = "",
+    color = "#428850",
+    cterm_color = "65",
+    name = "Zsh"
+  }
+ };
+ -- globally enable different highlight colors per icon (default to true)
+ -- if set to false all icons will have the default icon's color
+ color_icons = true;
+ -- globally enable default icons (default to false)
+ -- will get overriden by `get_icons` option
+ default = true;
+ -- globally enable "strict" selection of icons - icon will be looked up in
+ -- different tables, first by filename, and if not found by extension; this
+ -- prevents cases when file doesn't have any extension but still gets some icon
+ -- because its name happened to match some extension (default to false)
+ strict = true;
+ -- set the light or dark variant manually, instead of relying on `background`
+ -- (default to nil)
+ variant = "light|dark";
+ -- same as `override` but specifically for overrides by filename
+ -- takes effect when `strict` is true
+ override_by_filename = {
+  [".gitignore"] = {
+    icon = "",
+    color = "#f1502f",
+    name = "Gitignore"
+  }
+ };
+ -- same as `override` but specifically for overrides by extension
+ -- takes effect when `strict` is true
+ override_by_extension = {
+  ["log"] = {
+    icon = "",
+    color = "#81e043",
+    name = "Log"
+  }
+ };
+ -- same as `override` but specifically for operating system
+ -- takes effect when `strict` is true
+ override_by_operating_system = {
+  ["apple"] = {
+    icon = "",
+    color = "#A2AAAD",
+    cterm_color = "248",
+    name = "Apple",
+  },
+ };
+}
+
+
+
+
+require("toggleterm").setup{}
+
